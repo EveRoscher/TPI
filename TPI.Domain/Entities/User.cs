6e80778a7d2f5282e8bdcc74e2605d618413d9b5
EveRoscher <evelyncarol@hotmail.es>
2026-05-01 18:22:13 -0300

    corrections

--- TPI.Domain/Entities/User.cs
+++ TPI.Domain/Entities/User.cs
@@ -6,7 +6,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty; //ver encriptado
+        public string Password { get; set; } = string.Empty; //ver tema encriptado
         public string? Phone { get; set; } //dato opcional, por defecto null
         public enum Status 
         {
